3d194c66ff54f5544265cce843a8f698000b6cfb
David Ariza <davidariza030@gmail.com>
2025-10-30 13:10:01 +0100

    Multiplicación

--- src/es/comares/daw/ed/Calculadora.java
+++ src/es/comares/daw/ed/Calculadora.java
@@ -5,6 +5,7 @@ public class Calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1 -> SUMA");
+		System.out.println("3 -> MULTIPLICACION");
 	}
 
 }
